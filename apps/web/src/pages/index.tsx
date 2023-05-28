bed83958986d084e717e5523f77060e2478f58a0
Jay Nam <siisee111@gmail.com>
2023-05-28 22:33:28 +0900

    feat: use useWhisper hook on mainpage

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -1,19 +1,70 @@
 import Head from "next/head";
 import { useEffect, useRef } from "react";
 import CurveAnimation from "../components/CurveAnimation";
+import useWhisper from "@chengsokdara/use-whisper";
 
 import { useAudioRecorder } from 'react-audio-voice-recorder';
+import axios from "axios";
 
 export default function Home() {
+
+  const onTranscribe = async (blob: Blob) => {
+    const formdata = new FormData();
+    const file = new File([blob], "speech.mp3", {
+      type: "audio/mpeg",
+    });
+    formdata.append("audioData", file, "speech.mp3");
+    formdata.append("model", "whisper-1");
+
+    const url = "http://localhost:9000/transcriptions";
+    const response = await axios.post(url, formdata, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    const { text } = await response.data
+    // you must return result from your server in Transcript format
+    return {
+      blob,
+      text,
+    }
+  }
+
+  const streamToServer = async (blob) => {
+    const formdata = new FormData();
+    const file = new File([blob], "speech.mp3", {
+      type: "audio/mpeg",
+    });
+    formdata.append("audioData", file, "speech.mp3");
+    formdata.append("model", "whisper-1");
+
+    const url = "http://localhost:9000/transcriptions";
+    const response = await axios.post(url, formdata, {
+      headers: { "Content-Type": "multipart/form-data" },
+    });
+    const { text } = await response.data
+    // you must return result from your server in Transcript format
+    return {
+      blob,
+      text,
+    }
+  }
+
   const {
+    recording,
+    speaking,
+    transcribing,
+    transcript,
+    pauseRecording,
     startRecording,
     stopRecording,
-    togglePauseResume,
-    recordingBlob,
-    isRecording,
-    isPaused,
-    recordingTime,
-  } = useAudioRecorder();
+  } = useWhisper({
+    // streaming: true,
+    whisperConfig: {
+      language: "ko"
+    },
+    // useCustomServer: true,
+    onTranscribe,
+    // onDataAvailable: streamToServer,
+  })
 
   const childRef = useRef({
     start: () => {},
@@ -53,16 +104,6 @@ export default function Home() {
     // TTS
   };
 
-  useEffect(() => {
-    if (!recordingBlob) return;
-
-    const link = document.createElement('a');
-    link.href = window.URL.createObjectURL(recordingBlob);
-    link.download = `${+new Date()}.wav`;
-    link.click();
-    // recordingBlob will be present at this point after 'stopRecording' has been called
-  }, [recordingBlob])
-
   return (
     <div
       className="flex min-h-screen flex-col items-center justify-center py-2 "
@@ -72,16 +113,21 @@ export default function Home() {
       onTouchEnd={() => process()}
     >
       <Head>
-        <title>OS-J</title>
+        <title>JANOT</title>
       </Head>
 
-      <main className="mx-auto w-auto px-4 pt-16 pb-8 sm:pt-24 lg:px-8">
+      <main className="mx-auto w-auto px-4">
         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-0">
           <CurveAnimation ref={childRef} />
         </div>
-        <h1 className="mt-24 mx-auto text-center text-2xl font-thin tracking-tight text-white sm:text-3xl lg:text-4xl xl:text-4xl">
-          OS-J
-        </h1>
+        <div className="flex min-h-screen flex-col items-center justify-between p-24">
+          <h1 className="text-center text-2xl font-thin tracking-tight text-white sm:text-3xl lg:text-4xl xl:text-4xl">
+            JANOT
+          </h1>
+          <p className="text-center text-2xl font-thin tracking-tight text-white">
+            {transcript.text}
+          </p>
+        </div>
       </main>
     </div>
   );
